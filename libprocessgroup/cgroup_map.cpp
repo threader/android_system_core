8a3ff4e2712c1c540cbed7a464a5ea00f9c722e1
threader <michael.heltne@gmail.com>
2025-07-05 03:15:01 +0200

    Revert "libprocessgroup: Add MaxActivationDepth"
    This reverts commit 28b37f27048679874f1e1185fe9f852d36872e73.
    
    Change-Id: I48664320957558e7fd4db3fceb57854b7b65228e

--- libprocessgroup/cgroup_map.cpp
+++ libprocessgroup/cgroup_map.cpp
@@ -28,7 +28,6 @@
 #include <android-base/strings.h>
 #include <cgroup_map.h>
 #include <processgroup/processgroup.h>
-#include <processgroup/util.h>
 
 using android::base::StartsWith;
 using android::base::StringPrintf;
@@ -217,13 +216,7 @@ int CgroupMap::ActivateControllers(const std::string& path) const {
         for (uint32_t i = 0; i < controller_count; ++i) {
             const ACgroupController* controller = ACgroupFile_getController(i);
             const uint32_t flags = ACgroupController_getFlags(controller);
-            uint32_t max_activation_depth = UINT32_MAX;
-            if (__builtin_available(android 36, *)) {
-                max_activation_depth = ACgroupController_getMaxActivationDepth(controller);
-            }
-            const int depth = util::GetCgroupDepth(ACgroupController_getPath(controller), path);
-
-            if (flags & CGROUPRC_CONTROLLER_FLAG_NEEDS_ACTIVATION && depth < max_activation_depth) {
+            if (flags & CGROUPRC_CONTROLLER_FLAG_NEEDS_ACTIVATION) {
                 std::string str("+");
                 str.append(ACgroupController_getName(controller));
                 if (!WriteStringToFile(str, path + "/cgroup.subtree_control")) {
--- libprocessgroup/setup/cgroup_descriptor.h
+++ libprocessgroup/setup/cgroup_descriptor.h
@@ -30,8 +30,7 @@ namespace cgrouprc {
 class CgroupDescriptor {
   public:
     CgroupDescriptor(uint32_t version, const std::string& name, const std::string& path,
-                     mode_t mode, const std::string& uid, const std::string& gid, uint32_t flags,
-                     uint32_t max_activation_depth);
+                     mode_t mode, const std::string& uid, const std::string& gid, uint32_t flags);
 
     const format::CgroupController* controller() const { return &controller_; }
     mode_t mode() const { return mode_; }
